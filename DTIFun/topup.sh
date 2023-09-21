6eb8ecdb7bc92e4661295d2e77fa29dc084cda08
Gong-Jun JI <77033347+jigongjun@users.noreply.github.com>
2023-09-21 10:22:59 +0800

    Add files via upload
    Support for topup function

---
+++ DTIFun/topup.sh
@@ -0,0 +1,3 @@
+topup --imain=data_appa_b0.nii --datain=para.txt --config=b02b0.cnf --out=Topup_Output 
+
+applytopup --imain=data.nii,datapa.nii --inindex=1,2 --datain=para.txt --topup=Topup_Output --out=my_hifi_data
