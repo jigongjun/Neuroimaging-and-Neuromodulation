9f76e3c9048b2d13d5cc5205de236215fc0d13b9
Gong-Jun JI <77033347+jigongjun@users.noreply.github.com>
2023-11-29 15:05:30 +0800

    Delete invpearson.m

--- invpearson.m
+++
@@ -1,5 +0,0 @@
-function r = invpearson(p,n)
-% from p-value to Pearson's linear correlation value . just return positive
-% corr value . r = [r,-r]; formula deduced by t-distribution: t = rho.*sqrt((n-2)./(1-rho.^2)).
-t = tinv ( 1-p/2, n-2) ;
-r = sqrt(t^2/(n-2 + t^2)) ;
